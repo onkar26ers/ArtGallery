dd2ec7942c5c31e7cb5943f4df37707eb178c0d7
onkar26ers <naiksonkar@gmail.com>
2020-08-20 00:15:12 +0530

    NavBar

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('show');
+    });
+    $('#signupButton').click(function(){
+        $('#signupModal').modal('show');
+    });
+});
